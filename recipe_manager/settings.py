98be2bf5f9880415f0c3eeaa6fead4fea17fe3be
Liam Chadwick <liam_2112@hotmail.co.uk>
2024-11-28 16:07:12 +0000

    added gunicorn & procfile

--- recipe_manager/settings.py
+++ recipe_manager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7)9tq^+6nd^mx3o2v4_nr+b4y_b4uabfiktxt6vt0=@54tk3u9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-liam2112-recipesite-skabzp623x5.ws.codeinstitute-ide.net'
