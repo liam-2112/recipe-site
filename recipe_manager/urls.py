3ca333aab1ca446c2a54b02682bd47e74a78f07a
Liam Chadwick <liam_2112@hotmail.co.uk>
2024-12-05 12:53:08 +0000

    added a generic view template

--- recipe_manager/urls.py
+++ recipe_manager/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include('home.urls'), name='home-urls'),
     path('admin/', admin.site.urls),
-    path('', include('home.urls'), name='home'),
+    
 ]
 
