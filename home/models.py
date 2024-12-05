64453a633d560637dbd614e43084bba757a12174
Liam Chadwick <liam_2112@hotmail.co.uk>
2024-12-05 11:20:58 +0000

    Created a comment model

--- home/models.py
+++ home/models.py
@@ -5,6 +5,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 
+# post models
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -16,6 +17,14 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+
+# comment models
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
@@ -24,3 +33,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
