1e71d98ce35fc6a95507b2da9d5b0ab783feb5a0
Liam Chadwick <liam_2112@hotmail.co.uk>
2024-12-05 16:45:55 +0000

    Added fixtures with json. stored locally for backup

--- home/models.py
+++ home/models.py
@@ -10,7 +10,9 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="home_posts"
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="home_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -27,9 +29,13 @@ class Post(models.Model):
 # comment models
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
+        Post, 
+        on_delete=models.CASCADE, 
+        related_name="comments")
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
