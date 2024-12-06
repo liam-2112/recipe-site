475ea83e7cb37a16f6dc776f0007dbdd2459b830
Liam Chadwick <liam_2112@hotmail.co.uk>
2024-12-06 14:59:32 +0000

    Changed static js directory location

--- recipe_manager/settings.py
+++ recipe_manager/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-liam2112-recipesite-skabzp623x5.ws.codeinstitute-ide.net',
@@ -139,8 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 
 
 # Default primary key field type
