a4c5bfbec5069c5c7b61e71de25f3b2e5cfeb3dc
Liam Chadwick <liam_2112@hotmail.co.uk>
2024-12-12 13:44:54 +0000

    deployment test

--- recipe_manager/settings.py
+++ recipe_manager/settings.py
@@ -30,10 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.path.isfile("env.py"):
-    DEBUG = True
-else:
-    DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-liam2112-recipesite-skabzp623x5.ws.codeinstitute-ide.net',
