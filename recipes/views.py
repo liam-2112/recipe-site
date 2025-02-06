aeeeaedbfdac7706fd8292dc09a9a2a4321a3add
liam-chadwick <liam_2112@hotmail.co.uk>
2025-02-06 20:04:54 +0000

    11 - changed the full views.py file. fourth change

--- recipes/views.py
+++ recipes/views.py
@@ -1,28 +1,30 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView
-from .models import MenuSection, MenuItem
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import MenuItem
+from .forms import RecipeForm
+from django.contrib.auth.decorators import login_required
 
+# Recipe List View with Pagination
+def recipe_list(request):
+    recipes = MenuItem.objects.all().order_by('-id')  # Latest recipes first
+    paginator = Paginator(recipes, 10)               # 10 recipes per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'recipes/recipes.html', {'page_obj': page_obj})
 
-# Create your views here.
-
-
-class MenuPage(TemplateView):
-    """
-    Displays menu page
-    """
-    template_name = 'recipes/recipes.html'
-
-def menu_view(request):
-    menu_sections = MenuSection.objects.all()
-    menu_items = MenuItem.objects.all().select_related('section')
-
-    context = {
-        "menu_sections" : menu_sections,
-        "menu_items" : menu_items,
-    }
-    
-    return render(request, 'recipes/recipes.html', context)
-
+# Recipe Detail View
 def recipe_detail(request, item_id):
-    item = get_object_or_404(MenuItem, id=item_id)
-    return render(request, 'recipe_detail.html', {'item': item})
+    recipe = get_object_or_404(MenuItem, id=item_id)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
+
+# Add New Recipe (Authenticated Users Only)
+@login_required
+def add_recipe(request):
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('recipe_list')
+    else:
+        form = RecipeForm()
+    return render(request, 'recipes/add_recipe.html', {'form': form})
